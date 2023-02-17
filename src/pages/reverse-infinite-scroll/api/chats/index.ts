88e168e517e00b930fa7a7814048ab70a3236f94
Cola <bcad1591@gmail.com>
2023-02-18 08:53:47 +0900

    reverse-infinite-scroll. chats api endpoint 수정

--- src/pages/reverse-infinite-scroll/api/chats/index.ts
+++ src/pages/reverse-infinite-scroll/api/chats/index.ts
@@ -1,7 +1,9 @@
 import type { GetChats } from './types';
 
+import { endpoints } from '~/endpoints';
+
 export const getChats: GetChats = (cursor) => {
-  const endpoint = `/infinite?cursor=${cursor}`;
+  const endpoint = `${endpoints.chats()}?cursor=${cursor}`;
 
   return fetch(endpoint).then((response) => response.json());
 };
