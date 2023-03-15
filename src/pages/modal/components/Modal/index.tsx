74ebf800c074afdb5a6289a58834eb5421ccc46f
Cola <bcad1591@gmail.com>
2023-03-15 18:39:03 +0900

    modal/ focus trap 추가

--- src/pages/modal/components/Modal/index.tsx
+++ src/pages/modal/components/Modal/index.tsx
@@ -7,6 +7,8 @@ import type {
 
 import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
+import { createElement as el } from '~/utils/dom';
+import { getFocusableElements } from '~/utils/focusable-selectors';
 
 import './styles.scss';
 
@@ -53,6 +55,7 @@ const Modal: FC<ModalProps> = ({
     };
   }, []);
 
+  // usePreviousFocusOnClose
   useEffect(() => {
     if (!isOpen) return;
 
@@ -63,6 +66,70 @@ const Modal: FC<ModalProps> = ({
     // 모달을 닫으면 포커스를 prevFocusElement로 이동시킨다.
     return () => {
       prevFocusElement?.focus?.();
+    };
+  }, [isOpen]);
+
+  // useTrapFocus
+  useEffect(() => {
+    if (!isOpen) return;
+    const $contentRoot = contentRootRef.current;
+    if (!$contentRoot) return;
+
+    const $$focusable = getFocusableElements($contentRoot);
+    const $firstFocusable = $$focusable.at(0) || $contentRoot;
+    const $lastFocusable = $$focusable.at(-1) || $contentRoot;
+
+    // 앞뒤로 요소를 추가한다.
+    const $modalTrapStart = el('div', { class: 'trap-start', tabindex: '0' });
+    const $modalTrapEnd = el('div', { class: 'trap-end', tabindex: '0' });
+    document.body.insertAdjacentElement('afterbegin', $modalTrapStart);
+    document.body.insertAdjacentElement('beforeend', $modalTrapEnd);
+
+    const handleFocusTrapStart = (e: globalThis.FocusEvent) => {
+      e.preventDefault();
+      $firstFocusable?.focus();
+    };
+
+    const handleFocusTrapEnd = (e: globalThis.FocusEvent) => {
+      e.preventDefault();
+      $lastFocusable?.focus();
+    };
+
+    const handleTabWithShiftOnFirstFocusable = (
+      e: globalThis.KeyboardEvent
+    ) => {
+      e.preventDefault();
+      const withShift = e.shiftKey;
+      const isTabKey = e.key === 'Tab';
+
+      if (isTabKey && withShift) {
+        $contentRoot.focus();
+        $lastFocusable?.focus?.();
+      }
+    };
+
+    const handleTabOnLastFocusable = (e: globalThis.KeyboardEvent) => {
+      e.preventDefault();
+      const isTabKey = e.key === 'Tab';
+
+      if (isTabKey) {
+        $contentRoot.focus();
+        $firstFocusable?.focus?.();
+      }
+    };
+
+    $firstFocusable?.addEventListener(
+      'keydown',
+      handleTabWithShiftOnFirstFocusable
+    );
+    $lastFocusable?.addEventListener('keydown', handleTabOnLastFocusable);
+
+    $modalTrapStart.addEventListener('focus', handleFocusTrapStart);
+    $modalTrapEnd.addEventListener('focus', handleFocusTrapEnd);
+
+    return () => {
+      $modalTrapStart.remove();
+      $modalTrapEnd.remove();
     };
   }, [isOpen]);
 
