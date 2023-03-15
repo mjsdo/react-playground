ce62bdb032efa02618a37d1ed4d383ca1a0f044d
Cola <bcad1591@gmail.com>
2023-03-16 01:09:52 +0900

    modal/ getFocusableElements 추가

---
+++ src/utils/focusable-selectors.ts
@@ -0,0 +1,32 @@
+/* https://github.com/KittyGiraudel/focusable-selectors/blob/main/index.js */
+
+const not = {
+  inert: ':not([inert]):not([inert] *)',
+  negTabIndex: ':not([tabindex^="-"])',
+  disabled: ':not(:disabled)',
+};
+
+export const focusableSelectors = [
+  `a[href]${not.inert}${not.negTabIndex}`,
+  `area[href]${not.inert}${not.negTabIndex}`,
+  `input:not([type="hidden"]):not([type="radio"])${not.inert}${not.negTabIndex}${not.disabled}`,
+  `input[type="radio"]${not.inert}${not.negTabIndex}${not.disabled}`,
+  `select${not.inert}${not.negTabIndex}${not.disabled}`,
+  `textarea${not.inert}${not.negTabIndex}${not.disabled}`,
+  `button${not.inert}${not.negTabIndex}${not.disabled}`,
+  `details${not.inert} > summary:first-of-type${not.negTabIndex}`,
+  // Discard until Firefox supports `:has()`
+  // See: https://github.com/KittyGiraudel/focusable-selectors/issues/12
+  // `details:not(:has(> summary))${not.inert}${not.negTabIndex}`,
+  `iframe${not.inert}${not.negTabIndex}`,
+  `audio[controls]${not.inert}${not.negTabIndex}`,
+  `video[controls]${not.inert}${not.negTabIndex}`,
+  `[contenteditable]${not.inert}${not.negTabIndex}`,
+  `[tabindex]${not.inert}${not.negTabIndex}`,
+].join(',');
+
+export const getFocusableElements = (base: HTMLElement) =>
+  [...base.querySelectorAll(focusableSelectors)] as HTMLElement[];
+
+export const getFocusableElement = (base: HTMLElement) =>
+  base.querySelector<HTMLElement>(focusableSelectors);
